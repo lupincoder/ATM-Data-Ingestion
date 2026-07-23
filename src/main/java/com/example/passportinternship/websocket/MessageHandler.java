d730b9b8cb83e963e576fc7084b5a7404050a8e4
lupincoder <stock.lupin1@gmail.com>
2026-07-23 08:04:32 -0700

    feat: added basic websocket useable with postman

---
+++ src/main/java/com/example/passportinternship/websocket/MessageHandler.java
@@ -0,0 +1,19 @@
+package com.example.passportinternship.websocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+
+@Component
+public class MessageHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
+        System.out.println("Server received: " + message);
+
+    }
+
+}
---
+++ src/main/java/com/example/passportinternship/websocket/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.example.passportinternship.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final MessageHandler messageHandler;
+
+    public WebSocketConfig(MessageHandler messageHandler) {
+        this.messageHandler = messageHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(messageHandler, "/myHandler");
+    }
+
+}
